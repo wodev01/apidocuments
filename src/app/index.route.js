9abc6c1783da7037c429280171ede6a6ba195d7b
vbjain <vipul.jain0@gmail.com>
2016-12-08 14:25:39 +0530

    dynamic route config

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,77 +7,44 @@
 
     /** @ngInject */
     function routerConfig($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('main', {
-                abstract: true,
-                url: "/",
-                templateUrl: 'app/main/main.html',
-                controller: 'MainController',
-                controllerAs: 'main'
-            })
-            .state('main.account', {
-                url: "account",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "account"}
-            })
-            .state('main.clients', {
-                url: "clients",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "clients"}
-            })
-            .state('main.dashboard', {
-                url: "dashboard",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "dashboard"}
-            })
-            .state('main.groups', {
-                url: "groups",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "groups"}
-            })
-            .state('main.locations', {
-                url: "locations",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "location"}
-            })
-            .state('main.shop-locations', {
-                url: "shop-locations",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "shop-location"}
-            })
-            .state('main.payments', {
-                url: "payments",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "payment"}
-            })
-            .state('main.users', {
-                url: "users",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "users"}
-            })
-            .state('main.campaigns', {
-                url: "campaigns",
-                templateUrl: 'app/template/template.html',
-                controller: 'TemplateController',
-                controllerAs: 'template',
-                data: {"api-name": "campaigns"}
+
+        var statesArray = ['main'];
+        angular.forEach(cargly.api['config'], function (obj) {
+            statesArray.push(obj.sref);
+        });
+        //add a new Function to the object $stateProvider
+        $stateProvider.states = function (statesArr, obj) {
+            obj = {};
+            var configData = cargly.api['config'];
+
+            angular.forEach(statesArr, function (state) {
+                if (state === 'main') {
+                    obj = {};
+                    obj.abstract = true;
+                    obj.url = "/";
+                    obj.templateUrl = 'app/main/main.html';
+                    obj.controller = 'MainController';
+                    obj.controllerAs = 'main';
+                } else {
+                    angular.forEach(configData, function (ob) {
+                        if (state === ob.sref) {
+                            obj = {};
+                            obj.url = ob.name;
+                            obj.name = ob.name;
+                            obj.templateUrl = 'app/template/template.html';
+                            obj.controller = 'TemplateController';
+                            obj.controllerAs = 'template';
+                            obj.data = {"api-name": ob.name};
+                        }
+                    });
+                }
+                $stateProvider.state(state, obj);
             });
+            return $stateProvider;
+        };
+
+        $stateProvider
+            .states(statesArray, {});
 
         $urlRouterProvider.otherwise('/dashboard');
     }
